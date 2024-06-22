5f8b55e81d87a1121c9d844c67c01f07376f39f0
Aziz Koumenji <azizkoumenji@protonmail.com>
2024-06-22 13:19:52 +0100

    Commited backend

---
+++ backend/config.js
@@ -0,0 +1,2 @@
+export const PORT = 5555
+export const mongoDBURL = "mongodb+srv://azizkoumenji:79KVQRGeTtRYoz@cluster.xztvlxm.mongodb.net/books?retryWrites=true&w=majority&appName=Cluster"
---
+++ backend/index.js
@@ -0,0 +1,37 @@
+import express, { response } from "express"
+import { PORT, mongoDBURL } from "./config.js"
+import mongoose from "mongoose";
+import booksRoute from "./routes/booksRoute.js"
+
+const app = express();
+
+app.use(express.json()) //JSON parsing middleware.
+app.use(cors()) //Middleware for handeling CORS (allowing all origins).
+
+/*
+You can allow custom origins (client address) with:
+app.use(cors({
+  origin: "https://localhost:3000",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type"]
+}))
+*/
+
+app.get("/", (request, response) => {
+  console.log(request)
+  return response.status(234).send("Welcome to BookStore!")
+})
+
+app.use("/books", booksRoute)
+
+mongoose.connect(mongoDBURL).then(() => {
+  console.log("Connected to database!")
+  app.listen(PORT, () => {
+    console.log(`Listening to port: ${PORT}`)
+  })
+}).catch((error) => {
+  console.log("Error connecting to database!")
+  console.log(error)
+})
+
+
